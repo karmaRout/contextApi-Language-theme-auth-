af92ff9bcbee0a5386e6a748aa885070aca05276
imprintnext-karma <kenton@imprintnext.com>
2026-03-31 14:21:42 +0530

    fallback rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import './App.css'
 import NavBar from './components/NavBar'
 import { Route, Routes } from 'react-router-dom'
 //import Profile from './pages/Profile'
-import Dashboard from './pages/Dashboard'
+//import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
 import PrivateRouter from './components/PrivateRouter'
-const Profile = lazy(() => import('./pages/Profile'))
+import Loading from './components/Loading'
+const Profile = lazy(() => 
+  new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve(import('./pages/Profile'))
+    }, 4000)
+  })
+)
+const Dashboard = lazy(() =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(import('./pages/Dashboard')), 4000);
+  })
+);
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,10 +28,18 @@ function App() {
      <NavBar />
      <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
+        
+          <Route  path="/dashboard" 
+            element={
+              <Suspense fallback={<Loading />}>
+                <Dashboard />
+              </Suspense>
+            }>
+          </Route>
+        
         <Route path="/profile" element={
           <PrivateRouter>
-            <Suspense fallback={<div>Loading</div>}>
+            <Suspense fallback={<Loading />}>
               <Profile />
             </Suspense>
           </PrivateRouter>
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loader"></div>
+      <p className="loading-text">Loading, please wait...</p>
+    </div>
+  );
+}
+
+export default Loading;
